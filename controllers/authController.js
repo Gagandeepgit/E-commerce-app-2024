334e48c9fe28bc44b8d3a43f6c153257aae9cf18
Gagandeepgit <119419910+Gagandeepgit@users.noreply.github.com>
2024-02-18 00:48:16 +0530

    Added MVC

---
+++ controllers/authController.js
@@ -0,0 +1,54 @@
+import { hashPassword } from "../helpers/authHelper.js";
+import userModels from "../models/userModels.js"
+
+export const registerController = async(req, res) => {
+    try{
+        const {name, email, password, phone, address} = req.body;
+
+        //validation
+        if(!name){
+            return res.send({error:'Name is required'});
+        }
+        if(!email){
+            return res.send({error:'Email is required'});
+        }
+        if(!password){
+            return res.send({error:'Password is required'});
+        }
+        if(!phone){
+            return res.send({error:'Phone Number is required'});
+        }
+        if(!address){
+            return res.send({error:'Address is required'});
+        }
+
+        const existinguser = await userModels.findOne({email});
+        //Check Existing user
+        if (existinguser) {
+            return res.status(200).send({
+                success:true,
+                message: 'Already Register Please login'
+            })
+        }
+
+        //register user
+        const hashedPassword = await hashPassword(password);
+
+        //save
+        const user = new userModels({name, email, password, phone, address, password:hashedPassword}).save();
+
+        res.status(201).send({
+            success:true,
+            message: 'User Register Successfully'
+        });
+
+    } catch (error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message: 'Error in Registration',
+            error
+        })
+    }
+}
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRouter from './routes/authRoute.js';
 
 //configure env
 dotenv.config(); //as it is in the root directory so we dont need to define oath inside bracket
@@ -17,8 +18,10 @@ connectDB();
 app.use(express.json());
 app.use(morgan('dev'));
 
-//rest api
+//routes
+app.use('api/v1/auth', authRouter);
 
+//rest api
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to E-Commerece app</h1>');
 })
