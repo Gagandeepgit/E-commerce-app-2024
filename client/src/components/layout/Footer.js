94d8896786f16c609ab52eee14afda9d781694a3
Gagandeep Patbandha <gagandeep.patbandha@hyscaler.com>
2024-04-27 17:41:37 +0530

    pages added

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
+    <div className='footer'>
       <h4 className='text-center'>All Right Reserved &copy; Techinfoyt</h4>
       <p className="text-center mt-3">
-        <Link to="/aboot">About</Link>|<Link to="/contact">Contact</Link>|<Link to="/policy">Privacy Policy</Link>
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|<Link to="/policy">Privacy Policy</Link>
       </p>
     </div>
   )
