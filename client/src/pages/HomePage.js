858fe9f8e1269df9b8d8874f6958b8c1ace09ca8
Gagandeep Patbandha <gagandeep.patbandha@hyscaler.com>
2024-06-23 01:51:50 +0530

    login page added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
+import React from 'react';
+import Layout from '../components/layout/Layout';
+import { useAuth } from '../context/auth';
 
 const HomePage = () => {
+  const [auth, setAuth] =useAuth()
   return (
     <Layout title={'Best offeres'}>
       <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   )
 }
