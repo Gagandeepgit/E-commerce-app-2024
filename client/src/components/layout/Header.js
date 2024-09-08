3561e1e454edc5ee539165b928ec80ebcac1e972
Gagandeep Patbandha <gagandeep.patbandha@hyscaler.com>
2024-09-09 00:16:13 +0530

    Backend frontend connection

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { HiShoppingBag } from "react-icons/hi2";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user:null,
+      token:'',
+    })
+    localStorage.removeItem('auth');
+    toast.success('Logout Successfully');
+  }
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -33,16 +45,26 @@ const Header = () => {
                   category
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/register" className="nav-link" href="#">
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/login" className="nav-link" href="#">
-                  Login
-                </NavLink>
-              </li>
+              {
+                !auth.user ? (<>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link" href="#">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link" href="#">
+                      Login
+                    </NavLink>
+                  </li>
+                </>) : (<>
+                  <li className="nav-item">
+                    <NavLink onClick= {handleLogout} to="/login" className="nav-link" href="#">
+                      Logout
+                    </NavLink>
+                  </li>
+                </>)
+              }
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link" href="#">
                   Cart (0)
